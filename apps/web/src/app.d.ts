8043a60c735b9b51501ffa51d22be95223683a8f
VidiAgungAdhyaksa <kyoshirocommander@gmail.com>
2026-03-30 16:11:57 +0700

    feat: define global App.Locals interface with user type

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,13 +1,13 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {
+			user?: {
+				id: string;
+				email: string;
+				role: 'admin' | 'viewer';
+			};
+		}
 	}
 }
 
-export {};
+export { };
