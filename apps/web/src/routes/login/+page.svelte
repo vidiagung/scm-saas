5cea3069fdc543bf0ef22a4483305cc2ce2d6d7e
VidiAgungAdhyaksa <kyoshirocommander@gmail.com>
2026-04-03 16:12:09 +0700

    refactor(auth): update branding and adjust login page styling

--- apps/web/src/routes/login/+page.svelte
+++ apps/web/src/routes/login/+page.svelte
@@ -3,17 +3,15 @@
 	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
 </script>
 
-<div
-	class="dark flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10"
->
+<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
 	<div class="flex w-full max-w-sm flex-col gap-6">
-		<a href="##" class="flex items-center gap-2 self-center font-medium text-white">
+		<a href="##" class="flex items-center gap-2 self-center font-medium">
 			<div
 				class="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground"
 			>
 				<GalleryVerticalEndIcon class="size-4" />
 			</div>
-			Acme Inc.
+			SCM-SaaS.
 		</a>
 		<LoginForm />
 	</div>
