de59310c3f4abb21196f73e5a12da357f1d7a5b0
VidiAgungAdhyaksa <kyoshirocommander@gmail.com>
2026-04-04 21:30:13 +0700

    feat(ui): initialize ModeWatcher in root layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,9 +2,14 @@
 	import './layout.css';
 	import 'leaflet/dist/leaflet.css';
 	import favicon from '$lib/images/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
 {@render children()}
